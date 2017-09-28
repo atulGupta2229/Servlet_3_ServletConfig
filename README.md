0a17fb2e1dd1287a5ed0354e5fa1dde5ae6b1ac3
atulGupta2229 <31149725+atulGupta2229@users.noreply.github.com>
2017-09-28 22:16:52 +0530

    Example of ServletConfig Interface

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Servlet_3_ServletConfig
 Example of Using ServletConfig Interface.
+
+This particular repository contains the example of ServletConfig Interface.
+The examples show how to use the four methods of the ServletConfig Interface.
+The four methods are:
+	public String getInitParameter();
+	public Enumeration getInitParameterNames();
+	public String getServletName();
+	public ServletContext getServletContext();
---
+++ Servlet_3(ServletConfig)/src/sConfigPack/ConfigServlet1.java
@@ -0,0 +1,39 @@
+package sConfigPack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class ConfigServlet1 extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException, ServletException{
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		ServletConfig cfg=getServletConfig();
+		
+		/*Using getInitParmeter() method*/
+		out.println(cfg.getInitParameter("val1").toString()+"<br>");
+		
+		/*Using getInitParameterNames() method*/
+		Enumeration<String> IPnames=cfg.getInitParameterNames();
+		while(IPnames.hasMoreElements()) {
+			String ipn=IPnames.nextElement().toString();
+			out.println("Param Name:"+ipn+"<br>");
+		}
+		
+		/*Using getServletName() method*/
+		String sName=cfg.getServletName().toString();
+		out.println("Servlet Name:"+sName+"<br>");
+		
+		/*Using getServletContex()*/
+		ServletContext ctx=cfg.getServletContext();
+		out.println(ctx.getInitParameter("Context").toString());
+	}
+}
